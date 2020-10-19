e7b4f3b297e75499819c79b85826850ad1d3b247
James Barclay <futureimperfect@users.noreply.github.com>
2020-10-19 00:41:37 -0400

    Added deploy script, temporarily updated baseURL, and deployed site.

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+THEME=cactus
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+if [ -z "$THEME" ]; then
+    hugo
+else
+    hugo -t "$THEME"
+fi
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
